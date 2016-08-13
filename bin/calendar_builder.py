83ff8b7b1e62cf61959cbbbbd69b32e2f802794d
Reha Sterbin <reha@omniti.com>
2016-08-13 03:43:05 +0000

    Actually calculate the end dates (nb: does not work with tuesdays_before yet)

--- bin/calendar_builder.py
+++ bin/calendar_builder.py
@@ -28,41 +28,50 @@ except:
     print "Cannot connect to the database"
     sys.exit(2)
 
-CALC_YEAR = 2014
+# Calculate for the church year starting with Advent 2014
+CALC_YEAR = 2012
 
 # Start by pinning down Easter and Christmas
-EASTER = easter(CALC_YEAR)
-CHRISTMAS = datetime.date(CALC_YEAR, 12, 25)
+CHRISTMAS1 = datetime.date(CALC_YEAR, 12, 25)
+EASTER = easter(CALC_YEAR + 1)
+CHRISTMAS2 = datetime.date(CALC_YEAR + 1, 12, 25)
 
 # Calculate the season boundries
 class boundary_algorithms:
     "Algorithms for calculating season boundaries"
 
     @staticmethod
-    def days_before():
+    def days_before(holiday_date, number):
         "Finds a number of days before the given holiday"
-        return 'Testing days_before'
+        return holiday_date - datetime.timedelta(days=number)
 
     @staticmethod
-    def days_after():
+    def days_after(holiday_date, number):
         "Finds a number of days after the given holiday"
-        return 'Testing days_after'
+        return holiday_date + datetime.timedelta(days=number)
 
     @staticmethod
-    def weeks_before():
+    def weeks_before(holiday_date, number):
         "Finds a number of weeks before the given holiday"
-        return 'Testing weeks_before'
+        return holiday_date - datetime.timedelta(weeks=number)
 
     @staticmethod
-    def weeks_after():
+    def weeks_after(holiday_date, number):
         "Finds a number of weeks after the given holiday"
-        return 'Testing weeks_after'
+        return holiday_date + datetime.timedelta(weeks=number)
 
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 cur.execute("select name, code, color, calculate_from, algorithm, distance from liturigal_seasons order by sort_order")
 seasons = cur.fetchall()
 
+holiday = CHRISTMAS1
 for row in seasons:
-    print row['name']
-    print getattr(boundary_algorithms, row['algorithm'])()
+    print row['name'] + ' // ' + row['calculate_from'] + ' // ' + row['algorithm'] + ' // ' + str(row['distance'])
+    if row['calculate_from'] == 'easter':
+        holiday = EASTER
+    elif row['calculate_from'] == 'christmas':
+        if holiday == EASTER:
+            holiday = CHRISTMAS2
+    end_date = getattr(boundary_algorithms, row['algorithm'])(holiday, row['distance'])
+    print 'Ends: ' + end_date.strftime('%m/%d/%Y')
 

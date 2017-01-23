c5bfeb1d77861b00fcb366ef7b8f823895a3aa3a
Reha Sterbin <reha@omniti.com>
2017-01-23 06:26:49 +0000

    Added floating feasts for Parish Requiem

--- database/migrations/20160810132316_stmarys_precedence.sql
+++ database/migrations/20160810132316_stmarys_precedence.sql
@@ -6,6 +6,7 @@ INSERT INTO observance_types (name, precedence) VALUES ('Major Feast (can be cel
 INSERT INTO observance_types (name, precedence) VALUES ('Feast (transfers from Sunday)', 40); -- 3
 INSERT INTO observance_types (name, precedence) VALUES ('Commemoration', 50); -- 4
 INSERT INTO observance_types (name, precedence) VALUES ('Major Feast (celebrated on the nearest Sunday)', 30); -- 5
+INSERT INTO observance_types (name, precedence) VALUES ('Lesser Commemoration', 55); -- 6
 
 -- rambler down
 
--- database/migrations/20170108223353_stmarys_floating_feasts.sql
+++ database/migrations/20170108223353_stmarys_floating_feasts.sql
@@ -18,6 +18,22 @@ INSERT INTO floating_feasts (name, code, otype_id, placement_index,
 INSERT INTO floating_feasts (name, code, otype_id, placement_index, algorithm, color)
     VALUES ('The First Book of Common Prayer, 1549', 'first-bcp', 4, 1, 'first_bcp', 'white');
 
+INSERT INTO floating_feasts (name, code, otype_id, placement_index, algorithm, color,
+    valid_start)
+    VALUES ('Parish Requiem', 'parish-requiem', 4, 1, 'parish_requiem', 'black',
+    '2016-01-01 00:00:00' AT TIME ZONE 'America/New_York');
+
+INSERT INTO floating_feasts (name, code, otype_id, placement_index, algorithm, color,
+    valid_start, valid_end)
+    VALUES ('Parish Requiem', 'parish-requiem', 6, 1, 'parish_requiem_5_skip', 'black',
+    '2015-01-01 00:00:00' AT TIME ZONE 'America/New_York',
+    '2015-12-31 23:59:59' AT TIME ZONE 'America/New_York');
+
+INSERT INTO floating_feasts (name, code, otype_id, placement_index, algorithm, color,
+    valid_end)
+    VALUES ('Parish Requiem', 'parish-requiem', 6, 1, 'parish_requiem_4_skip', 'black',
+    '2014-12-31 23:59:59' AT TIME ZONE 'America/New_York');
+
 -- rambler down
 
 TRUNCATE TABLE floating_feasts RESTART IDENTITY;

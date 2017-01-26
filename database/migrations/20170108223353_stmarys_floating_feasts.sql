454f8c0744c9976e1a2ac341e0b4da432e7e0afa
Reha Sterbin <reha@omniti.com>
2017-01-26 02:07:30 +0000

    Changeover for Of Our Lady occurred with Advent 2012, not January 2013

--- database/migrations/20170108223353_stmarys_floating_feasts.sql
+++ database/migrations/20170108223353_stmarys_floating_feasts.sql
@@ -6,14 +6,14 @@ INSERT INTO floating_feasts (name, code, otype_id, placement_index,
         valid_start)
     VALUES ('Of Our Lady', 'of-our-lady', 4, 1,
         'of_our_lady', 'white',
-        '2013-01-01 00:00:00' AT TIME ZONE 'America/New_York');
+        '2012-12-01 00:00:00' AT TIME ZONE 'America/New_York');
 
 INSERT INTO floating_feasts (name, code, otype_id, placement_index,
         algorithm, color,
         valid_end)
     VALUES ('Of Our Lady', 'of-our-lady', 4, 1,
         'of_our_lady_old', 'white',
-        '2012-12-31 23:59:59' AT TIME ZONE 'America/New_York');
+        '2012-11-30 23:59:59' AT TIME ZONE 'America/New_York');
 
 INSERT INTO floating_feasts (name, code, otype_id, placement_index, algorithm, color)
     VALUES ('The First Book of Common Prayer, 1549', 'first-bcp', 4, 1, 'first_bcp', 'white');

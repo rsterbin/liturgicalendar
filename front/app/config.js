ee4c222edbb7c11f1be2dcb7d6a96c2506f0652b
Reha Sterbin <reha@omniti.com>
2017-03-13 04:15:39 +0000

    Derive config data from env vars using a dot-env file

---
+++ front/app/config.js
@@ -0,0 +1,19 @@
+/**
+ * This lib reads a file in the project root named `.env`. It should be a
+ * sh-style file of variables, each line being `KEY=VALUE` pairs. It parses the
+ * file and merges the contents into the process.env map.
+ */
+if (!global.GLOBAL_OVERRIDE_LOAD_ENVIRONMENT_VARIABLES) {
+    require("dotenv").load();
+    global.GLOBAL_OVERRIDE_LOAD_ENVIRONMENT_VARIABLES = true;
+}
+
+module.exports = {
+    database: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_USER_PASSWORD
+    }
+};
